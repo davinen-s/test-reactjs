cea4943f82032b39b90f6b2b6eeffe020a4235f2
davinen-s <davinen.s@gmail.com>
2018-04-30 02:11:22 +0500

    react essentials test

---
+++ react-essentials-lynd/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { hello, goodbye } from "./lib";
+
+const style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'verdana'
+}
+
+render(
+  <div>
+    {hello}
+    {goodbye}
+  </div>,
+  document.getElementById('react-container')
+)
